45c18dfa9e4beac453086e161296c2ac8b8f58fc
ncar285 <carlier.nicholas@gmail.com>
2024-01-30 16:06:30 +1300

    import lusitana font and apply to subheading

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css'
+import { lusitana } from '@/app/ui/fonts';
 
 export default function Page() {
   return (
@@ -22,7 +23,9 @@ export default function Page() {
           className={styles.shape}
         />
 
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <p 
+            className={`${lusitana.className} antialiased text-xl text-gray-800 md:text-3xl md:leading-normal`}
+          >
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
