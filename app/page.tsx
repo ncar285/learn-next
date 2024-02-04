ebf1ecb806f9abda526ca7a36273aa6b06a63191
ncar285 <carlier.nicholas@gmail.com>
2024-02-04 17:03:10 +1300

    add logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,20 @@
-import AcmeLogo from '@/app/ui/acme-logo';
+// import AcmeLogo from '@/app/ui/acme-logo';
+// import Logo from '@/app/ui/logo.svg';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css'
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
+import KnightLogo from './ui/chess-logo';
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        <AcmeLogo />
+        {/* </> */}
+        {/* <img src={Logo} alt="Chess by Nico knight logo" /> */}
+        {/* <AcmeLogo /> */}
+        <KnightLogo/>
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
