2d0156fe1add4e1fc12e6d3c8345e598019a1152
ncar285 <carlier.nicholas@gmail.com>
2024-02-05 18:21:22 +1300

    header changes from chess by nico to chess

--- app/ui/chess-logo.tsx
+++ app/ui/chess-logo.tsx
@@ -9,7 +9,8 @@ export default function KnightLogo() {
     >
       {/* <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" /> */}
       <Logo className="h-12 w-12 mr-2" />
-      <p className="text-[44px]">Chess</p>
+      <p className="text-[44px] hidden custom:block">Chess by Nico</p>
+      <p className="text-[44px] block custom:hidden">Chess</p>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,6 +26,14 @@ const config: Config = {
         },
       },
     },
+    screens: {
+      'custom': '420px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
   },
   plugins: [require('@tailwindcss/forms')],
 };
